27bfeb3ac0f26657d85e4b13985751ae5023bfd1
Kjeni <kacper.blazejewski88@gmail.com>
2023-04-22 18:56:03 +0200

    Dodanie zad1

---
+++ zad1.py
@@ -0,0 +1,8 @@
+imie = input("Podaj imię: ")
+wiek = int(input("Ile masz lat?  "))
+if wiek < 32:
+    print("Mów mi Python, mam 32 lata. Witaj w moim świecie" + imie +".  Jesteś młodszy ode mnie")
+else:
+    print("Mów mi Python, mam 32 lata. Witaj w moim świecie"+ imie +". Jesteś starszy ode mnie")
+
+
