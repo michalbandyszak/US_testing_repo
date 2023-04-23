fd81b8cba9008e930e752e66a31443d40a0ac218
Vladyslav Maksymov <vmaksymovk@gmail.com>
2023-04-23 11:46:18 +0200

    moja branch z plikiem Zadania1-4

---
+++ Zadania1-4.py
@@ -0,0 +1,43 @@
+
+
+# Zadanie 1 *************
+
+
+# a = input("Input name")
+# b = input("Input  age")
+
+
+
+# Zadanie 2 ***********
+
+
+# age = int(b)
+# if age > 32 :
+#     print("Mow mi python, mam " + b +  " lat. Witaj w moim swiecie " + a + " Jestes starszy ode mnie")
+
+# else:
+#     print("Mow mi python, mam " + b + " lat. Witaj w moim swiecie " + a + " Jestes mlodszy ode mnie")
+
+
+# Zadanie 3 *****************
+
+
+
+
+# inputUser = input("Input: ")
+
+# reversed_string = inputUser[::-1]
+# print(reversed_string)
+
+
+# Zadanie 4 ****************
+
+# a = input("Input: ")
+# print(a.count(' ') + 1)
+
+
+
+print(12242)
+print(42352652)
+
+print(12322332)
