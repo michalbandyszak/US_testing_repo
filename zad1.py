78d4e956dac6b59a4a8f6301bb8e4051a6b796e2
Jakub <jakubdera091@gmail.com>
2023-04-23 15:51:16 +0200

    commit1

---
+++ zad1.py
@@ -0,0 +1,6 @@
+a = input("podaj imię")
+b = int(input("podaj swój wiek"))
+if b > 32:
+    print("Mów mi Python, mam 32 lat. Witaj w moim świecie ",a,". Jesteś starszy ode mnie.")
+else:
+    print("Mów mi Python, mam 32 lat. Witaj w moim świecie ",a,". Jesteś młodszy ode mnie.")
