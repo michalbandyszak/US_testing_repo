28963919200c2bdd32231aea705228cef72ce527
Damian Górski <rezet9800@gmail.com>
2023-04-23 16:25:03 +0200

    commit

---
+++ zad1.py
@@ -0,0 +1,8 @@
+a = input('Podaj imie: ')
+b = int(input('Podaj wiek: '))
+x = 32
+if b < x:
+    print('Mów mi Python, mam 32 lat. Witaj w moim świecie ',a,'. Jesteś młodszy ode mnie')
+else:
+    print('Mów mi Python, mam 32 lat. Witaj w moim świecie ',a,'. Jesteś starszy ode mnie')
+
