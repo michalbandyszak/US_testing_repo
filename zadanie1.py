de380898aae8499370fc3313676fc73c2a86d665
PiotrPogorzelski01 <p.pogorzelski2606@gmail.com>
2023-04-26 00:10:17 +0200

    zadanie1

---
+++ zadanie1.py
@@ -0,0 +1,10 @@
+imie=input("Podaj imie: ")
+wiek=input("Podaj wiek: ")
+x=2023-1989
+
+print("Mów mi Python, mam",x,"lat","\nWitaj w moim świecie",imie)
+
+if x>int(wiek):
+    print("Jesteś starszy ode mnie")
+else:
+    print("Jesteś młodszy ode mnie")
