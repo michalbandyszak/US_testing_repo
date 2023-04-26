cd063886b060f503b921bf544d77b790e3fe1705
JulaMatykiewicz <131374459+JulaMatykiewicz@users.noreply.github.com>
2023-04-26 18:37:03 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello")
+imie = input('Podaj swoję imie')
+wiek = int(input('Podaj swój wiek'))
+if wiek > 32:
+    print("Mów mi Python, mam 32 lat. Witaj w moim świecie " + imie + " jesteś starszy ode mnie.")
+else:
+    print("Mów mi Python, mam 32 lat. Witaj w moim świecie " + imie + " jesteś młodszy ode mnie.")
