4767ca467884c7ed1fa48e94db836049980c53dc
lukijanki <121889156+lukijanki@users.noreply.github.com>
2023-04-26 19:33:29 +0200

    wrzucilem zad1

---
+++ zad1.py
@@ -0,0 +1,6 @@
+name = input('Podaj imie;')
+age = int(input('Podaj wiek;'))
+if age > 32:
+    print("Mów mi Python, mam 32 lat. Witaj w moim świecie " + name + " Jesteś starszy ode mnie")
+else:
+    print("Mów mi Python, mam 32 lat. Witaj w moim świecie " + name + " Jesteś młodszy ode mnie")
