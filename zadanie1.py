6cd6b3c35442aedf6caec0486adefb1fbe23362b
Karolina Brzozowska <karolina@macbook-air-karolina.home>
2023-04-28 14:57:25 +0200

    dodaniezadania

---
+++ zadanie1.py
@@ -0,0 +1,6 @@
+name = input("Podaj swoje imie: ")
+age = int(input("Podaj swój wiek: "))
+if age < 32:
+    print("Mów mi Python, mam 32 lat. Witaj w moim świecie " + name + " Jesteś młodszy ode mnie.")
+else:
+    print("Mów mi Python, mam 32 lat. Witaj w moim świecie " + name + " Jesteś starszy ode mnie")
