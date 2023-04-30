d89460527957eb5437dd75715ce34038b0b9df9e
jcobooo <131374551+jcobooo@users.noreply.github.com>
2023-04-30 21:20:39 +0200

    Add files via upload

---
+++ zad1.py
@@ -0,0 +1,8 @@
+imie = input("Podaj imię: ")
+wiek = input("Podaj wiek: ")
+
+wiek = int(wiek)
+if (wiek > 32):
+    print("Mów mi Python, mam 32 lata. Witaj w moim świecie " + imie + ". Jesteś starszy ode mnie.")
+else:
+    print("Mów mi Python, mam 32 lata. Witaj w moim świecie " + imie + ". Jesteś młodszy ode mnie.")
